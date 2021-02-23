2ada5ff20c2ff0382586778db563ffd8bfc674f5
Grant Lin <grant.lin@42-q.com>
2021-02-23 14:37:04 +0800

    Initial commit

---
+++ src/main/java/com/markerhub/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.markerhub.controller;
+
+
+import com.markerhub.common.lang.Result;
+import com.markerhub.entity.User;
+import com.markerhub.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author 关注公众号：MarkerHub
+ * @since 2021-02-22
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/{id}")
+    public Object test(@PathVariable("id") Long id) {
+        return userService.getById(id);
+    }
+
+    @PostMapping("/save")
+    public Result save(@Validated @RequestBody User user) {
+        return Result.succ(user);
+    }
+}
---
+++ src/main/java/com/markerhub/entity/Blog.java
@@ -0,0 +1,51 @@
+package com.markerhub.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.time.LocalDate;
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author 关注公众号：MarkerHub
+ * @since 2021-02-22
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName(value = "m_blog", schema = "spc")
+public class Blog implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    private Long userId;
+
+    @NotBlank(message = "标题不能为空")
+    private String title;
+
+    @NotBlank(message = "摘要不能为空")
+    private String description;
+
+    @NotBlank(message = "内容不能为空")
+    private String content;
+
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private LocalDate created;
+
+    private Integer status;
+
+}
